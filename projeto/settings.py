bb11e26f93de8520e85b5a280c65665ecb35668b
Carlos Marques <dev.carlosmarques@gmail.com>
2024-02-08 23:59:23 +0000

    .

--- projeto/settings.py
+++ projeto/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['20.201.113.178']
 
 
 # Application definition
